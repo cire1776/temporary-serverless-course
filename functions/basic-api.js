ff7089fbb62c80d51f07eb5f1e6970567de83b2b
cire1776 <cire1776@gmail.com>
2021-02-14 17:06:33 -0500

    correct header to headers

--- functions/basic-api.js
+++ functions/basic-api.js
@@ -1,8 +1,8 @@
 const items = require('../assets/data')
 exports.handler = async (event, context) => {
     return {
-        header: {
-            'Access-Control-Allow-Origin': '*'
+        headers: {
+            'Access-Control-Allow-Origin': '*',
         },
         statusCode: 200,
         body: JSON.stringify(items),
