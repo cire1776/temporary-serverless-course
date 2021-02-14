021850b06f1785155a4f5d65bbd9c8ed87761de0
cire1776 <cire1776@gmail.com>
2021-02-14 16:58:48 -0500

    add CORS header

--- functions/basic-api.js
+++ functions/basic-api.js
@@ -1,7 +1,10 @@
 const items = require('../assets/data')
 exports.handler = async (event, context) => {
     return {
+        header: {
+            'Access-Control-Allow-Origin': '*'
+        },
         statusCode: 200,
-        body: JSON.stringify(items)
+        body: JSON.stringify(items),
     }
 }
